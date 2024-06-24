e3915eae4c38f2cf2f1ff6ea5f0dc71c27ab1616
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-06-24 12:38:35 -0400

    Update sample menu to include UUIDs for rendering on DOM

--- src/db.jsx
+++ src/db.jsx
@@ -5,19 +5,22 @@ import Bruchetta from './assets/bruchetta.svg';
 const food1 = {
     title: "Greek Salad",
     img: GreekSaladImg,
-    description: "This refreshing Greek salad recipe with cucumber, tomatoes, feta cheese, and an easy oregano and olive oil dressing makes a standout summer salad!"
+    description: "This refreshing Greek salad recipe with cucumber, tomatoes, feta cheese, and an easy oregano and olive oil dressing makes a standout summer salad!",
+    key: crypto.randomUUID()
 }
 
 const food2 = {
     title: "Lemon Dessert",
     img: LemonDessert,
-    description: "This Lemon Lush Dessert is made up of shortbread crust that's layered with a lemon cream cheese filling, lemon pudding, and whipped cream."
+    description: "This Lemon Lush Dessert is made up of shortbread crust that's layered with a lemon cream cheese filling, lemon pudding, and whipped cream.",
+    key: crypto.randomUUID()
 }
 
 const food3 = {
     title: "Bruchetta",
     img: Bruchetta,
-    description: "Bruschetta is a classic Italian appetizer that is easy to make at home. Toasted bread is topped with a mixture of juicy tomatoes, garlic, Parmesan cheese, fresh basil, and balsamic."
+    description: "Bruschetta is a classic Italian appetizer that is easy to make at home. Toasted bread is topped with a mixture of juicy tomatoes, garlic, Parmesan cheese, fresh basil, and balsamic.",
+    key: crypto.randomUUID()
 }
 
 const sampleMenu = [food1, food2, food3];
