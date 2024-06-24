f8403aac56b84df42c4727e3d55b5b6f70beecd5
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-06-23 22:38:24 -0400

    Update styling of various components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className="section-title">
             <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/reservations'>Reservations</Link>
